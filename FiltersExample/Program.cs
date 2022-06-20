652dbf335136dcae596d7dd690742357834175f2
BrVenturaU <brandonventura16@gmail.com>
2022-06-20 09:13:44 -0600

    Add Startup class.

--- FiltersExample/Program.cs
+++ FiltersExample/Program.cs
@@ -1,42 +1,12 @@
+using FiltersExample;
 using FiltersExample.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddScoped<ActionFilterExample>(); // Filtro de controlador o de acción
-builder.Services.AddScoped<MetadaActionFilter>(); // Filtro de controlador o de acción
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
-builder.Services.AddControllers();
-/*
- * Para aplicar el filtro de forma global.
-builder.Services.AddControllers(options => options.Filters.Add(new MetadaActionFilter())); 
- 
-*/
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Dev", conf =>
-    {
-        conf.AllowAnyHeader()
-        .AllowAnyMethod().AllowAnyOrigin();
-    });
-});
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseCors("Dev");
-app.UseAuthorization();
-
-app.MapControllers();
-
+startup.Configure(app, app.Environment);
 app.Run();
---
+++ FiltersExample/Startup.cs
@@ -0,0 +1,58 @@
+﻿using FiltersExample.Filters;
+
+namespace FiltersExample
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddScoped<ActionFilterExample>(); // Filtro de controlador o de acción
+            services.AddScoped<MetadaActionFilter>(); // Filtro de controlador o de acción
+
+            services.AddControllers();
+            /*
+             * Para aplicar el filtro de forma global.
+            builder.Services.AddControllers(options => options.Filters.Add(new MetadaActionFilter())); 
+
+            */
+
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Dev", conf =>
+                {
+                    conf.AllowAnyHeader()
+                    .AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("Dev");
+            app.UseAuthorization();
+
+            app.UseEndpoints(config =>
+            {
+                config.MapControllers();
+            });
+        }
+    }
+}
